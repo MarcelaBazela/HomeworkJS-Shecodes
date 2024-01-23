27a99c5d5d21b8880c680e5b1a65d065c095a1f0
Marcela Bazela <116001582+MarcelaBazela@users.noreply.github.com>
2024-01-23 12:00:43 +0100

    Update file.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-
+  function changeCity() {
+        let citytName = prompt("What city do you live in?");
+        let temperature = prompt("What temperature is it?");
+        if (temperature < 0) {
+          let h1 = document.querySelector("h1");
+          h1.innerHTML =
+            "😔 <br/> Currently " + temperature + "°C in " + citytName;
+        } else {
+          let h1 = document.querySelector("h1");
+          h1.innerHTML =
+            "😀 <br/> Currently " + temperature + "°C in " + citytName;
+        }
+      }
+      let contactButton = document.querySelector("button");
+      contactButton.addEventListener("click", changeCity);
